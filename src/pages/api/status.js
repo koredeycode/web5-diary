00e3b93988ce89ab27ce9369f0d1a4d9d95b4704
koredeycode <koredey4u@gmail.com>
2023-11-30 17:19:18 +0100

    api

---
+++ src/pages/api/status.js
@@ -0,0 +1,11 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res
+    .status(200)
+    .json({
+      name: 'Reflectify Application',
+      tagline: 'decentralizing your thoughts and reflections',
+      status: 'active',
+    });
+}
